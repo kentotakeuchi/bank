9c6774caa5b5e076829bdc217a7ce8b14139c3a8
kentotakeuchi <ideal.kt.reality@gmail.com>
2019-06-30 17:45:47 -0700

    no message

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,9 @@
 //////////////////////////////////////////////////////
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter
-} from 'react-router-dom';
-import {
-  Provider
-} from 'react-redux';
-import {
-  createStore,
-  applyMiddleware,
-  compose,
-  combineReducers
-} from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
 import './index.scss';
@@ -25,22 +16,20 @@ import assetReducer from './store/reducers/assetReducer';
 console.log(`process.env.REACT_APP_URL`, process.env.REACT_APP_URL);
 //////////////////////////////////////////////////////
 
+
 ////////////////////////////////////////////
 /// REDUX
-const composeEnhancers =
-  process.env.NODE_ENV === 'development' ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ :
-    null || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
   auth: authReducer,
   asset: assetReducer
 });
 
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
 
 const app = (
   <Provider store={store}>
@@ -51,6 +40,10 @@ const app = (
 );
 
 ReactDOM.render(app, document.getElementById('root'));
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
