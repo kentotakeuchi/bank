23ddda2c60b283282461ad476bbc6590a1de5413
kentotakeuchi <ideal.kt.reality@gmail.com>
2019-07-02 16:33:04 -0700

    make it better

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // built in
 const path = require('path');
 const fs = require('fs');
+const enforce = require('express-sslify');
 // const https = require('https');
 
 // third party
@@ -17,11 +18,13 @@ const authRoutes = require('./routes/auth');
 const assetRoutes = require('./routes/asset');
 ///////////////////////////////////////////////////////
 
-
 // db
-const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-20l9d.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true`;
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${
+  process.env.MONGO_PASSWORD
+}@cluster0-20l9d.mongodb.net/${
+  process.env.MONGO_DEFAULT_DATABASE
+}?retryWrites=true`;
 console.log(`MONGODB_URI`, MONGODB_URI);
-
 
 console.log(`process.env.NODE_ENV`, process.env.NODE_ENV);
 
@@ -31,25 +34,25 @@ const app = express();
 // const privateKey = fs.readFileSync('server.key');
 // const certificate = fs.readFileSync('server.cert');
 
-
 // app.use(bodyParser.urlencoded({ extended: true })); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 });
-
 
 // routes
 app.use('/api/auth', authRoutes);
 app.use('/api/asset', assetRoutes);
-
 
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, 'access.log'),
@@ -59,17 +62,16 @@ const accessLogStream = fs.createWriteStream(
 app.use(helmet());
 app.use(compression());
 app.use(morgan('combined', { stream: accessLogStream }));
-
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 // for production. front + back
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'client/build')));
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
   console.log(`res`, res);
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
-
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 // error handling
 app.use((error, req, res, next) => {
@@ -80,9 +82,9 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, data: data });
 });
 
-
 // db
-mongoose.connect(MONGODB_URI)
+mongoose
+  .connect(MONGODB_URI)
   .then(result => {
     // server
     const port = process.env.PORT || 3001;
@@ -96,8 +98,8 @@ mongoose.connect(MONGODB_URI)
                     ***/
 
     const server = app.listen(port, () => {
-                      console.log(`Express server listening on port ${port}`);
-                    });
+      console.log(`Express server listening on port ${port}`);
+    });
   })
   .catch(error => {
     console.log(`error`, error);
